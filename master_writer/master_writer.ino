487f5b670926ea1704fd36599f402ec92e7f932e
Athul Asokan Thulasi <athulat.002@gmail.com>
2016-09-02 19:08:30 -0500

    Updated the IIC slave and master to match AD5933, read, write to AD5933 (emulator) register done

--- master_writer/master_writer.ino
+++ master_writer/master_writer.ino
@@ -1,19 +1,117 @@
-// Wire Master Writer
-// by Nicholas Zambetti <http://www.zambetti.com>
-
-// Demonstrates use of the Wire library
-// Writes data to an I2C/TWI slave device
-// Refer to the "Wire Slave Receiver" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
+// IIC Master device that intefaces with AD5933 I2C interface
+// Based on Wire Master Writer by Nicholas Zambetti <http://www.zambetti.com>
+// Author        : Athul Asokan Thulasi
+// Created on    : September 1, 2016
+// Last Modified : September 2, 2016
 
 
 #include <Wire.h>
 
 #define SLAVE_ID1 0x0D
 
+
+void setup() {
+  Wire.begin(); // join i2c bus (address optional for master)
+  Serial.begin(9600);
+  Serial.println("Master IIC device, Arduino Uno");
+  Serial.println(" ");
+  Serial.println(" ");
+}
+
+
+void loop() 
+{
+  start:
+  byte reg_adrs = 0x00;
+  byte data = 0x00;
+  byte write_cmd = 0x00;
+  byte read_cmd = 0x01;
+  byte cmd = 0xff;
+  uint8_t slave_adrs = SLAVE_ID1;
+  char adrs_buf[2];
+  char data_buf[2];
+  char cmd_buf[1];
+  byte read_data;
+  
+// Asking weather to read or write
+  Serial.print("Read (1) / Write (0) : ");
+  while(Serial.available() < 1);
+  Serial.readBytes(cmd_buf,1);
+  Serial.println(cmd_buf[0]);
+  if (cmd_buf[0] == '0')
+   cmd = write_cmd;
+  else if (cmd_buf[0] == '1')
+   cmd = read_cmd;
+  else
+  {
+    Serial.println ("Invalid Command: Write = 0, Read = 1");
+    goto start;
+  }
+  
+// Getting Write Address from user
+  Serial.print("Write/Read Address (2 byte, HEX): ");
+  Serial.flush();
+  while(Serial.available() < 2);
+  Serial.readBytes(adrs_buf,2); 
+  reg_adrs = char2hex(adrs_buf);
+  Serial.println(reg_adrs,HEX);
+  if(!(((reg_adrs>=0x80)&&(reg_adrs<=0x8B))||(reg_adrs==0x8F)||((reg_adrs>=0x93)&&(reg_adrs<=0x97))))
+  {
+    Serial.println("Invalid register address ! (0x80-0x8B;0x8F;0x93-0x97)");
+    goto start;
+  }
+  
+  if(cmd == 0)
+  {
+   //Getting write data from user
+   Serial.print("Write Data(2 byte, HEX): ");
+   Serial.flush();
+   while(Serial.available() < 2);
+   Serial.readBytes(data_buf,2);
+   data = char2hex(data_buf);
+   Serial.println(data,HEX);
+  }
+  // IIC Write or Read
+
+  if(cmd == 0)
+  send_write_command(slave_adrs,reg_adrs,data);
+  else if(cmd == 1)
+  {
+  read_data = send_read_command(slave_adrs,reg_adrs);
+  Serial.print("Received value (hex):");
+  Serial.println(read_data,HEX);
+  }
+  Serial.println(" ");
+  Serial.println(" ");
+}
+
+
+// Function to write one byte to slave device
+void send_write_command(uint8_t slave_adrs, byte reg_adrs,byte data)
+{
+  Wire.beginTransmission(slave_adrs);                 
+  //Wire.write(slave_adrs);
+  Wire.write(reg_adrs);
+  Wire.write(data);
+  Wire.endTransmission();      // stop transmitting
+  delay(500);
+}
+
+
+//Function to read one byte from slave device
+byte send_read_command(uint8_t slave_adrs,byte reg_adrs)
+{
+  byte adrs_ptr = 0xB0;
+  Wire.beginTransmission(slave_adrs);                 
+  //Wire.write(slave_adrs);
+  Wire.write(adrs_ptr);
+  Wire.write(reg_adrs);
+  Wire.endTransmission();      // stop transmitting
+  delay(500);
+  Wire.requestFrom(slave_adrs,uint8_t(1));
+  while(Wire.available()<1);
+  return Wire.read();  
+}
 
 // Function to convert a 2 char array (conatining 2 hex digits) to a single byte hexadecimal number.
 byte char2hex(char buf[])
@@ -63,73 +161,3 @@ byte char2hex(char buf[])
   //Serial.print("MSB: "); Serial.print(msb,HEX);Serial.print(" LSB: ");Serial.println(lsb,HEX);
   return ((msb*16)+lsb);
 }
-
-
-
-void setup() {
-  Wire.begin(); // join i2c bus (address optional for master)
-  Serial.begin(9600);
-  Serial.println("Master IIC device, Arduino Uno");
-  Serial.println(" ");
-  Serial.println(" ");
-}
-
-
-
-void loop() 
-{
-  byte adrs = 0x00;
-  byte data = 0x00;
-  byte write_cmd = 0x00;
-  byte read_cmd = 0x01;
-  byte cmd = 0xff;
-  
-  char adrs_buf[2];
-  char data_buf[2];
-  char cmd_buf[1];
-  
-  
-// Asking weather to read or write
-  Serial.print("Read (1) / Write (0) : ");
-  while(Serial.available() < 1);
-  Serial.readBytes(cmd_buf,1);
-  Serial.println(cmd_buf[0]);
-  if (cmd_buf[0] == '0')
-   cmd = write_cmd;
-  else if (cmd_buf[0] == '1')
-   cmd = read_cmd;
-  else
-    Serial.println ("Invalid Command: Write = 0, Read = 1");
-  
-// Getting Write Address from user
-  Serial.print("Write/Read Address (2 byte, HEX): ");
-  Serial.flush();
-  while(Serial.available() < 2);
-  Serial.readBytes(adrs_buf,2); 
-  adrs = char2hex(adrs_buf);
-  Serial.print("Read(hex):");
-  Serial.println(adrs,HEX);
-
-//Getting write data from user
-  Serial.print("Write Data(2 byte, HEX): ");
-  Serial.flush();
-  while(Serial.available() < 2);
-  Serial.readBytes(data_buf,2);
-  data = char2hex(data_buf);
-  Serial.print("Read(hex) :");
-  Serial.println(data,HEX);
-  send_command(adrs,cmd,data);
-  Serial.println(" ");
-  Serial.println(" ");
-}
-
-void send_command(byte adrs,byte cmd,byte data)
-{
-  Wire.beginTransmission(SLAVE_ID1);                 
-  Wire.write(adrs);
-  Wire.write(cmd);
-  Wire.write(data);
-  Wire.endTransmission();      // stop transmitting
-  delay(500);
-}
-
--- slave_receiver/slave_receiver.ino
+++ slave_receiver/slave_receiver.ino
@@ -1,7 +1,8 @@
 // I2C slave device that emulates the functioning of the AD5933 I2C Interface
 // Based on Wire Slave Receiver by Nicholas Zambetti <http://www.zambetti.com>
-// Author      : Athul Asokan Thulasi
-// Created on  : September 1, 2016
+// Author         : Athul Asokan Thulasi
+// Created on     : September 1, 2016
+// Last modified  : September 2, 2016
 
 #include <Wire.h>
 
@@ -38,189 +39,12 @@ byte reg_96 = 0;
 byte reg_97 = 0;
 /*******************************************/
 
-void execute_cmd(byte adrs, byte cmd, byte data)
-{
-  if(cmd == 0x01) // Read
-  {
-    switch (adrs) 
-    {
-    case 0x80:
-      Wire.write(reg_80) ;
-      Serial.print("Register 80 Value: ");
-      Serial.println(reg_80,HEX);
-      break;
-    case 0x81:
-      Wire.write(reg_81) ;
-       Serial.print("Register 81 Value: ");
-      Serial.println(reg_81,HEX);
-      break;
-    case 0x82:
-      Wire.write(reg_82) ;
-       Serial.print("Register 82 Value: ");
-      Serial.println(reg_82,HEX);
-      break;
-    case 0x83:
-      Wire.write(reg_83) ;
-       Serial.print("Register 83 Value: ");
-      Serial.println(reg_83,HEX);
-      break;
-    case 0x84:
-      Wire.write(reg_84) ;
-       Serial.print("Register 84 Value: ");
-      Serial.println(reg_84,HEX);
-      break;
-    case 0x85:
-      Wire.write(reg_85) ;
-       Serial.print("Register 85 Value: ");
-      Serial.println(reg_85,HEX);
-      break;
-    case 0x86:
-      Wire.write(reg_86) ;
-      Serial.print("Register 86 Value: ");
-      Serial.println(reg_86,HEX);
-      break;
-    case 0x87:
-      Wire.write(reg_87) ;
-      Serial.print("Register 87 Value: ");
-      Serial.println(reg_87,HEX);
-      break;
-    case 0x88:
-      Wire.write(reg_88) ;
-      Serial.print("Register 88 Value: ");
-      Serial.println(reg_88,HEX);
-      break;
-    case 0x89:
-      Wire.write(reg_89) ;
-      Serial.print("Register 89 Value: ");
-      Serial.println(reg_89,HEX);
-      break;
-    case 0x8A:
-      Wire.write(reg_8A) ;
-      Serial.print("Register 8A Value: ");
-      Serial.println(reg_8A,HEX);
-      break; 
-    case 0x8B:
-      Wire.write(reg_8B) ;
-      Serial.print("Register 8B Value: ");
-      Serial.println(reg_8B,HEX);
-      break; 
-    case 0x8F:
-      Wire.write(reg_8F) ;
-      Serial.print("Register 8F Value: ");
-      Serial.println(reg_8F,HEX);
-      break;
-    case 0x92:
-      Wire.write(reg_92) ;
-      Serial.print("Register 92 Value: ");
-      Serial.println(reg_92,HEX);
-      break;
-    case 0x93:
-      Wire.write(reg_93) ;
-      Serial.print("Register 93 Value: ");
-      Serial.println(reg_93,HEX);
-      break;
-    case 0x94:
-      Wire.write(reg_94) ;
-      Serial.print("Register 94 Value: ");
-      Serial.println(reg_94,HEX);
-      break;
-    case 0x95:
-      Wire.write(reg_95) ;
-      Serial.print("Register 95 Value: "); 
-      Serial.println(reg_95,HEX);
-      break;
-    case 0x96:
-      Wire.write(reg_96) ;
-      Serial.print("Register 96 Value: "); 
-      Serial.println(reg_96,HEX);
-      break;
-    case 0x97:
-      Wire.write(reg_97) ;
-      Serial.print("Register 97 Value: "); 
-      Serial.println(reg_97,HEX);
-      break;
-    default:
-      Serial.println("Invalid Address !: Read failed");
-    break;
-  }
-  }
-  else if (cmd == 0x00) // Write
-  {
-    switch (adrs) 
-    {
-    case 0x80:
-      reg_80 = data ;
-      break;
-    case 0x81:
-      reg_81 = data ;
-      break;
-    case 0x82:
-      reg_82 = data ;
-      break;
-    case 0x83:
-      reg_83 = data ;
-      break;
-    case 0x84:
-      reg_84 = data ;
-      break;
-    case 0x85:
-      reg_85 = data ;
-      break;
-    case 0x86:
-      reg_86 = data ;
-      break;
-    case 0x87:
-      reg_87 = data ;
-      break;
-    case 0x88:
-      reg_88 = data ;
-      break;
-    case 0x89:
-      reg_89 = data ;
-      break;
-    case 0x8A:
-      reg_8A = data ;
-      break; 
-    case 0x8B:
-      reg_8B = data ;
-      break; 
-    case 0x8F:
-      reg_8F = data ;
-      break;
-    case 0x92:
-      reg_92 = data ;
-      break;
-    case 0x93:
-      reg_93 = data ;
-      break;
-    case 0x94:
-      reg_94 = data ;
-      break;
-    case 0x95:
-      reg_95 = data ;
-      break;
-    case 0x96:
-      reg_96 = data ;
-      break;
-    case 0x97:
-      reg_97 = data ;
-      break;
-    default:
-      Serial.println("Invalid Address !: Write failed");
-    break;
-  }
-    
-  }
-  else
-  {
-    Serial.println(" Invalid Command (W=0,R=1)");
-  }
-}
-
+byte data_reg = 0xff;
 
 void setup() {
   Wire.begin(0x0D);                // join i2c bus with address 0x0D
   Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent);
   Serial.begin(9600);           // start serial for output
 }
 
@@ -229,29 +53,207 @@ void loop()
   delay(100);
 }
 
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
+// Function that executes whenever data is received from master 
 void receiveEvent(int howMany) 
 {
   Serial.print("Received ");
   Serial.print(howMany);
   Serial.println(" bytes");
-   if (Wire.available() == 3)  //adrs,cmd,data
+   if (Wire.available() == 2)  //adrs,data OR adrs_ptr,adrs
    {
-    byte adrs = Wire.read(); 
-    byte cmd  = Wire.read();
-    byte data = Wire.read();
-    Serial.print("Address: ");
-    Serial.print(adrs,HEX);
-    Serial.print("  Command (W=0,R=1): ");
-    Serial.print(cmd,HEX);
-    Serial.print("  Data: ");
-    Serial.println(data,HEX);
-    execute_cmd(adrs,cmd,data);
+    byte byte1 = Wire.read(); 
+    byte byte2  = Wire.read();
+    Serial.print("Byte1: ");
+    Serial.print(byte1,HEX);
+    Serial.print("  Byte2: ");
+    Serial.println(byte2,HEX);
+    execute_cmd(byte1,byte2);
    }
    else
    {
     Serial.println("Invalid Command String :: Check number of bytes sent !");
    }
  }
- 
+
+
+// Function that executes when the master request for data.
+void requestEvent()
+{
+ Wire.write(data_reg);
+}
+
+// Function that writes to register OR updates the data_regiser with requested data
+void execute_cmd(byte byte1, byte byte2)
+{
+  if(byte1 == 0xB0)  // Address Pointer 
+  {
+    switch (byte2) // Address
+    {
+    case 0x80:
+      data_reg = reg_80; 
+      Serial.print("Register 80 Value: ");
+      Serial.println(reg_80,HEX);
+      break;
+    case 0x81:
+      data_reg = reg_81;
+       Serial.print("Register 81 Value: ");
+      Serial.println(reg_81,HEX);
+      break;
+    case 0x82:
+      data_reg = reg_82;
+       Serial.print("Register 82 Value: ");
+      Serial.println(reg_82,HEX);
+      break;
+    case 0x83:
+      data_reg = reg_83;
+       Serial.print("Register 83 Value: ");
+      Serial.println(reg_83,HEX);
+      break;
+    case 0x84:
+      data_reg = reg_84;
+       Serial.print("Register 84 Value: ");
+      Serial.println(reg_84,HEX);
+      break;
+    case 0x85:
+      data_reg = reg_85;
+       Serial.print("Register 85 Value: ");
+      Serial.println(reg_85,HEX);
+      break;
+    case 0x86:
+      data_reg = reg_86;
+      Serial.print("Register 86 Value: ");
+      Serial.println(reg_86,HEX);
+      break;
+    case 0x87:
+      data_reg = reg_87;
+      Serial.print("Register 87 Value: ");
+      Serial.println(reg_87,HEX);
+      break;
+    case 0x88:
+      data_reg = reg_88;
+      Serial.print("Register 88 Value: ");
+      Serial.println(reg_88,HEX);
+      break;
+    case 0x89:
+      data_reg = reg_89;
+      Serial.print("Register 89 Value: ");
+      Serial.println(reg_89,HEX);
+      break;
+    case 0x8A:
+      data_reg = reg_8A;
+      Serial.print("Register 8A Value: ");
+      Serial.println(reg_8A,HEX);
+      break; 
+    case 0x8B:
+      data_reg = reg_8B;
+      Serial.print("Register 8B Value: ");
+      Serial.println(reg_8B,HEX);
+      break; 
+    case 0x8F:
+      data_reg = reg_8F;
+      Serial.print("Register 8F Value: ");
+      Serial.println(reg_8F,HEX);
+      break;
+    case 0x92:
+      data_reg = reg_92;
+      Serial.print("Register 92 Value: ");
+      Serial.println(reg_92,HEX);
+      break;
+    case 0x93:
+      data_reg = reg_93;
+      Serial.print("Register 93 Value: ");
+      Serial.println(reg_93,HEX);
+      break;
+    case 0x94:
+      data_reg = reg_94;
+      Serial.print("Register 94 Value: ");
+      Serial.println(reg_94,HEX);
+      break;
+    case 0x95:
+      data_reg = reg_95;
+      Serial.print("Register 95 Value: "); 
+      Serial.println(reg_95,HEX);
+      break;
+    case 0x96:
+      data_reg = reg_96;
+      Serial.print("Register 96 Value: "); 
+      Serial.println(reg_96,HEX);
+      break;
+    case 0x97:
+      data_reg = reg_97;
+      Serial.print("Register 97 Value: "); 
+      Serial.println(reg_97,HEX);
+      break;
+    default:
+      Serial.println("Invalid Address !: Cannot issue a read to this Address");
+    break;
+  }
+  }
+  else  // Write
+  {
+    switch (byte1) 
+    {
+    case 0x80:
+      reg_80 = byte2 ;
+      break;
+    case 0x81:
+      reg_81 = byte2 ;
+      break;
+    case 0x82:
+      reg_82 = byte2 ;
+      break;
+    case 0x83:
+      reg_83 = byte2 ;
+      break;
+    case 0x84:
+      reg_84 = byte2 ;
+      break;
+    case 0x85:
+      reg_85 = byte2 ;
+      break;
+    case 0x86:
+      reg_86 = byte2 ;
+      break;
+    case 0x87:
+      reg_87 = byte2 ;
+      break;
+    case 0x88:
+      reg_88 = byte2 ;
+      break;
+    case 0x89:
+      reg_89 = byte2 ;
+      break;
+    case 0x8A:
+      reg_8A = byte2 ;
+      break; 
+    case 0x8B:
+      reg_8B = byte2 ;
+      break; 
+    case 0x8F:
+      reg_8F = byte2 ;
+      break;
+    case 0x92:
+      reg_92 = byte2 ;
+      break;
+    case 0x93:
+      reg_93 = byte2 ;
+      break;
+    case 0x94:
+      reg_94 = byte2 ;
+      break;
+    case 0x95:
+      reg_95 = byte2 ;
+      break;
+    case 0x96:
+      reg_96 = byte2 ;
+      break;
+    case 0x97:
+      reg_97 = byte2 ;
+      break;
+    default:
+      Serial.println("Invalid Address !: Write failed");
+    break;
+  }
+ }
+}
+
